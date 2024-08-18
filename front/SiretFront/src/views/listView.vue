91c5faba6da75081015af388f545902bc4a01dee
thomas <thomasmai.sanchez@gmail.com>
2024-08-18 15:14:56 +0200

    [Front] ajustement + sort

--- front/SiretFront/src/views/listView.vue
+++ front/SiretFront/src/views/listView.vue
@@ -6,15 +6,23 @@ import router from '../router'
 export default {
   data() {
     return {
-      items: []
+      items: [],
+      sortValue: 'null'
     }
   },
   created() {
     apiClient.getIndex().then(response => {
     this.items = response.data;
+
     }).catch(error => {
       console.error('There was an error fetching data from the API:', error);
     });
+  },
+
+  watch: {
+    sortValue(select) {
+      this.updateRelatedItems(select);
+    }
   },
 
   methods: {
@@ -23,6 +31,21 @@ export default {
       this.items = this.items.filter(item => item.id !== id); //supprimer en local
     }, 
 
+    updateRelatedItems(select){
+      if(select == 'nom'){
+        this.items.sort(function (a, b) {return a.Nom > b.Nom;})     
+      }
+      if(select == 'id'){
+        this.items.sort(function (a, b) {return a.id > b.id;})
+      }
+      if(select == 'SIRET'){
+        this.items.sort(function (a, b) {return a.SIRET > b.SIRET;})
+      }
+      if(select == 'SIREN'){
+        this.items.sort(function (a, b) {return a.SIREN > b.SIREN;})
+      }
+    },
+  
 
     editItem(id){
       router.push(`/${id}/edit`); 
@@ -47,7 +70,16 @@ export default {
   <ul>
         <button @click="RedirectAddEntreprise()">add new one</button>
         <button @click="RedirectDoc()">Doc</button>
+      <ul id="sort">
+        <label>sort by : </label>
+        <select v-model="sortValue" id="sortSelect">
+          <option  value="nom">nom</option>
+          <option  value="id">id</option>
+          <option  value="SIRET">SIRET</option>
+          <option  value="SIREN">SIREN</option>
 
+        </select>
+      </ul>
   </ul>
 
     <table class="table">
@@ -95,11 +127,38 @@ export default {
     border: none; /* Enlève la bordure par défaut */
     border-radius: 4px; /* Bordure arrondie */
     background-color: #10504d; /* Couleur de fond du bouton */
-    color: #fff; /* Couleur du texte du bouton */
+    color: #fff; 
     cursor: pointer; /* Curseur pointer pour le bouton */
     margin: 0 5px;
   }
   
+button:hover {
+  background-color: #ff0000; 
+  background: #ff0000;
+  color: #000000; 
+  margin: 0 5px;
+  padding: 10px;
+}
+
+#sort{
+  color: #fff; 
+
+}
+
+#sortSelect{
+    padding: 0;
+    margin: 0;
+    border: 1px solid #000000;
+    width: 110px;
+    border-radius: 3px;
+    overflow: hidden;
+    background-color: #10504d;
+    background: #10504d;
+    position: relative;
+    color: #fff; 
+
+}
+
   button:hover {
     background-color: #10504d; /* Couleur du bouton au survol */
   }
